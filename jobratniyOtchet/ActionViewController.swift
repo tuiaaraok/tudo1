829b1070532d86f6a288af167d4d5e75ab2ee6f4
Tuiaara <tuyara_ok@mail.ru>
2020-06-06 18:17:55 +0300

    Added func create constraints

--- jobratniyOtchet/ActionViewController.swift
+++ jobratniyOtchet/ActionViewController.swift
@@ -14,6 +14,7 @@ class ActionViewController: UIViewController {
     @IBOutlet var numberLabel: UILabel!
     @IBOutlet var doneButton: UIButton!
     
+    @IBOutlet var ostalosLabel: UILabel!
     @IBOutlet var pauseButton: UIButton!
     @IBOutlet var finishButton: UIButton!
 
@@ -28,15 +29,149 @@ class ActionViewController: UIViewController {
     
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        doneButton.layer.cornerRadius = doneButton.frame.width / 35
-        taskLabel.text = task
+         super.viewDidLoad()
+        
+        
+        taskLabel.translatesAutoresizingMaskIntoConstraints = false
+        numberLabel.translatesAutoresizingMaskIntoConstraints = false
+        ostalosLabel.translatesAutoresizingMaskIntoConstraints = false
+        doneButton.translatesAutoresizingMaskIntoConstraints = false
+        pauseButton.translatesAutoresizingMaskIntoConstraints = false
+        finishButton.translatesAutoresizingMaskIntoConstraints = false
        
+        
+        
+        doneButton.layer.cornerRadius = doneButton.frame.width / 35
         finishButton.isHidden = true
+        
+        taskLabel.text = task
         actionNumber = number
         numberLabel.text = String(actionNumber)
-      
+     
+        createConstraints()
+       
     }
+    
+     func createConstraints() {
+       
+        numberLabel.widthAnchor.constraint(
+            equalTo: view.widthAnchor,
+            multiplier: 1/2
+        ).isActive = true
+        
+        numberLabel.heightAnchor.constraint(
+            equalTo: view.heightAnchor,
+            multiplier: 1/4
+        ).isActive = true
+        
+        numberLabel.centerYAnchor.constraint(
+            equalTo: view.centerYAnchor,
+            constant: 30
+        ).isActive = true
+        
+        numberLabel.centerXAnchor.constraint(
+            equalTo: view.centerXAnchor
+        ).isActive = true
+        
+        
+        
+    
+        ostalosLabel.widthAnchor.constraint(
+            equalTo: view.widthAnchor,
+            multiplier: 1/2
+        ).isActive = true
+        
+        ostalosLabel.heightAnchor.constraint(
+            equalTo: view.heightAnchor,
+            multiplier: 1/10
+        ).isActive = true
+        
+        ostalosLabel.centerYAnchor.constraint(
+            equalTo: numberLabel.topAnchor,
+            constant: -50
+        ).isActive = true
+        
+        ostalosLabel.centerXAnchor.constraint(
+            equalTo: view.centerXAnchor
+        ).isActive = true
+        
+        
+        
+        
+        taskLabel.widthAnchor.constraint(
+            equalTo: view.widthAnchor,
+            multiplier: 1
+        ).isActive = true
+        
+        taskLabel.heightAnchor.constraint(
+            equalTo: view.heightAnchor,
+            multiplier: 1/2
+        ).isActive = true
+        taskLabel.centerYAnchor.constraint(
+            equalTo: ostalosLabel.topAnchor,
+            constant:  -55
+        ).isActive = true
+        taskLabel.centerXAnchor.constraint(
+            equalTo: view.centerXAnchor
+        ).isActive = true
+        
+        
+
+        
+        doneButton.widthAnchor.constraint(
+            equalTo: view.widthAnchor,
+            multiplier: 3/5
+        ).isActive = true
+        
+        doneButton.heightAnchor.constraint(
+            equalTo: view.heightAnchor,
+            multiplier: 1/15
+        ).isActive = true
+        
+        doneButton.centerYAnchor.constraint(
+            equalTo: numberLabel.bottomAnchor,
+            constant: 60
+        ).isActive = true
+        doneButton.centerXAnchor.constraint(
+            equalTo: view.centerXAnchor
+        ).isActive = true
+        
+        
+        
+        
+        pauseButton.widthAnchor.constraint(
+            equalTo: view.widthAnchor,
+            multiplier: 3/5
+        ).isActive = true
+        
+        pauseButton.heightAnchor.constraint(
+            equalTo: view.heightAnchor,
+            multiplier: 1/15
+        ).isActive = true
+        pauseButton.centerYAnchor.constraint(
+            equalTo: doneButton.bottomAnchor,
+            constant: 50
+        ).isActive = true
+        pauseButton.centerXAnchor.constraint(
+            equalTo: view.centerXAnchor
+        ).isActive = true
+        
+        
+        
+        
+        finishButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 3/5).isActive = true
+        finishButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/15).isActive = true
+        finishButton.centerYAnchor.constraint(equalTo: doneButton.bottomAnchor, constant: 50).isActive = true
+        finishButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        
+        }
+    
+    
+    
+    
+    
+    
       @IBAction func unwindSegue1 (_ sender: UIStoryboardSegue) {
         finishButton.isHidden = false
         pauseButton.isHidden = true
