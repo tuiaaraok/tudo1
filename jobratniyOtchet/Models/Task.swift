2b86b03adacdd2057c1af5386e81ab0bd4de45f6
Tuiaara <tuyara_ok@mail.ru>
2020-09-19 13:03:52 +0300

    Refreshing

--- jobratniyOtchet/Models/Task.swift
+++ jobratniyOtchet/Models/Task.swift
@@ -1,26 +1,20 @@
 //
-//  DataManager.swift
+//  Task.swift
 //  jobratniyOtchet
 //
-//  Created by Айсен Шишигин on 02/05/2020.
+//  Created by Айсен Шишигин on 19/09/2020.
 //  Copyright © 2020 Айсен Шишигин. All rights reserved.
 //
 
 import Foundation
 import RealmSwift
 
-
-var overDoValue = 0
+var overDoValue =  0
 
 class Task: Object {
     
-    @objc dynamic var task = ""
-    @objc dynamic var countOfTask = ""
-    @objc dynamic var overDo = 0
-    @objc dynamic var currentNumber = 0
+    @objc  dynamic  var task =  " "
+    @objc  dynamic  var countOfTask =  " "
+    @objc  dynamic  var overDo =  0
+    @objc  dynamic  var currentNumber =  0
 }
-
-
-
-
-
